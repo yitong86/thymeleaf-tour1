24dda1c87ef686e5e754ff3ebb96acd338af9a86
yitong86 <sophiajiang86@hotmail.com>
2023-02-09 12:09:04 -0500

    Initial commit

---
+++ src/main/java/com/example/thymeleaftour1/ThymeleafTour1Application.java
@@ -0,0 +1,13 @@
+package com.example.thymeleaftour1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafTour1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafTour1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/thymeleaftour1/ThymeleafTour1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thymeleaftour1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafTour1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
